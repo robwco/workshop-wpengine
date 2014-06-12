ebb036ae6db7a137f2dd78b3b482eaaba9ed05d1
Robert Williams <robwilliamsgd@gmail.com>
2014-06-11 23:13:47 -0700

    add nav

--- wp-content/themes/workshop_wp_theme/index.php
+++ wp-content/themes/workshop_wp_theme/index.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 <section id="content" role="main">
-<h3 class="masthead">Written by <a href="http://letsworkshop.com">Workshop</a>, a tiny newsletter and community for web design and development consultancies.</h3>
+<h3 class="masthead">Written by <a href="http://letsworkshop.com">Workshop</a>, a tiny community and newsletter for web design and development consultancies.</h3>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
 <?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+<p class="center small masthead">Curious about how I've helped other freelancers and shops? <a href="http://letsworkshop.com">See what my customers have said.</a></p>
+<p class="center small masthead">Ready to automate how you find leads daily? <a href="http://letsworkshop.com">Get my guide to an endless stream of clients.</a></p>
+<p class="center small masthead"><a href="http://letsworkshop.com">Read my story</a>, <a href="http://letsworkshop.com">read my essays</a>, and <a href="http://letsworkshop.com">follow me on twitter</a>.</p>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/workshop_wp_theme/nav-below.php
+++ wp-content/themes/workshop_wp_theme/nav-below.php
@@ -1,5 +1,2 @@
 <?php global $wp_query; if ( $wp_query->max_num_pages > 1 ) { ?>
-<p class="center small masthead">Curious about how I've helped other freelancers and shops? <a href="http://letsworkshop.com">See what my customers have said.</a></p>
-<p class="center small masthead">Ready to automate how you find leads daily? <a href="http://letsworkshop.com">Get my guide to an endless stream of clients.</a></p>
-<p class="center small masthead"><a href="http://letsworkshop.com">Read my story</a>, <a href="http://letsworkshop.com">read my essays</a>, and <a href="http://letsworkshop.com">follow me on twitter</a>.</p>
 <?php } ?>
