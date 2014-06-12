33eca7c8f6b397897b9e0e969fe2b5398e416c0b
Robert Williams <robwilliamsgd@gmail.com>
2014-06-11 23:18:21 -0700

    gravatar

--- wp-content/themes/workshop_wp_theme/entry-footer.php
+++ wp-content/themes/workshop_wp_theme/entry-footer.php
@@ -1,7 +1,5 @@
 <footer class="entry-footer">
-<p class="author">This essay was written by  <?php 
-   echo get_avatar( $id_or_email, $size = '30' ); 
-   ?> <?php the_author(); ?></p>
+<p class="author">This essay was written by  <?php echo get_avatar( get_the_author_email(), '30' ); ?> <?php the_author(); ?></p>
 <span class="tag-links"><?php the_tags(); ?></span>
 <?php if ( comments_open() ) {
 echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'workshop_wp_theme' ) ) . '</a></span>';
