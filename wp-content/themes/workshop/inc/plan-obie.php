1e141cafe9488933bf9d12d1286d75210408c6d4
Robert Williams <robwilliamsgd@gmail.com>
2014-07-15 15:52:17 -0700

    brennan and obie

---
+++ wp-content/themes/workshop/inc/plan-obie.php
@@ -0,0 +1,69 @@
+<div class="plan">
+
+
+	<h1 style="font-size: 42px;">
+		Try Workshop risk-free for 30 days
+	</h1>
+	<h2>
+		If you're not completely blown away, get a full refund.
+	</h2>
+	<div class="box-shadow">
+		<h2 class="center">
+			“Totally a no-brainer for most freelancers"
+		</h2><span class="quote-arrow"></span>
+	</div>
+	<p class="center">
+		<img src="/images/testimonials/ruben.png" class="testimonial-img"> – Ruben Gamez, Founder, <img src="../images/testimonials/bidsketch-logo.png" style="width:27px; border-radius:3px; vertical-align: -7px;"> Bidsketch
+	</p>
+
+	<p>
+		Perfect for generalists and studios who offer full-service consulting but not for those who don't want to do the work to find great gigs. You'll receive all agency, design, and development leads, every weekday. Usually about 10+ leads per email. Leads sent out Monday through Friday at 8:30 AM EST. <strong>For only $2.13 per day,</strong> you'll receive:
+	</p>
+	<p>
+		<strong>Agency and Team Gigs:</strong> Requests for proposals, Consulting gigs, Full-service design and development inquiries, Copywriting, Promotion, Project management.
+	</p>
+	<p>
+		<strong>Design Gigs:</strong> Wordpress design, Web design, UI design, Branding/Logo design, Graphic design, Motion graphics, Packaging, Art direction and branding, Infographics. Animation, 3D design, Interface design, Industrial design, User experience, and Interaction design
+	</p>
+	<p>
+		<strong>Development Gigs:</strong> Front-end development, Drupal development, Javascript development, Python and django development, PHP development, Back-end development, System administration, Web development, Game development, Mobile development, Ruby and rails development, CMS development, Image processing, HTML/CSS development, Magento development, Data development, .NET development.
+	</p>
+	<hr>
+	<p><strong>
+		As a special bonus you get access to my...
+	</strong></p>
+
+	<p style="margin-bottom:2em;"><img style="float: left; width: 130px; margin-right: 1em;" src="../images/marketing/community.png" alt=""> <strong>Workshop members-only community:</strong> A private discussion area where you can share tips and tricks, and be introduced to other freelancers and gigs in your area and around the world.</p>
+	<hr>
+	<h1>
+		All for about 30¢ per lead
+	</h1>
+	<p class="center">
+		12 payments of <strike>$64/month</strike> $60/month (with Obie Fernandez discount)
+	</p>
+
+	<div style="text-align: center; margin: 2em;">
+		<a href="https://workshop.memberful.com/checkout?plan=2236"><input type="submit" name="submit" value="MAKE LIFE EASIER, GET 2400+ LEADS A YEAR" data-drip-attribute="sign-up-button"></a>
+	</div>
+
+<hr>
+
+	<p><strong>What happens if I don't like it?</strong>
+	</p>
+	<p>
+		<span class="gaurantee">30-DAY</span> 100% Satisfaction Guarantee. Enjoy my leads for a full month, and if you don't like the service for any reason, send me an email and I'll issue you a refund. I don't want your money if you're not 100% blown away by my service.
+	</p>
+	<p>
+		<strong>Who are you?</strong>
+	</p>
+
+	<img style="border-radius: 50px; float: left; margin-right: 20px; height: 75px; width: 75px; display: inline-block;" src="../images/testimonials/robert-williams.png" alt="">
+
+	<p>I’m Robert Williams, an experienced freelancer who knows exactly which prospects will be an asset to your business. I’ve worked with startups, ad agencies, and small and large businesses.
+	</p>
+	<p style="margin-bottom:2em;">
+		I made Workshop because I wished something like it was available for me, and I'm excited to share it with you. I've been featured in places like UCSD, Wired, Copyhackers, Gizmodo, Bidsketch, and the Dieline. I'd like to help lower your blood pressure by providing you a great service. See you inside!
+	</p>
+</div>
+</section>
+</div>
