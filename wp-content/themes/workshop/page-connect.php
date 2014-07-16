7615722aa516ee3ec59aeba912cf942b74e3fea3
Robert Williams <robwilliamsgd@gmail.com>
2014-07-15 17:11:54 -0700

    connect

--- wp-content/themes/workshop/page-connect.php
+++ wp-content/themes/workshop/page-connect.php
@@ -13,22 +13,44 @@ Template Name: Post to Workshop
  * @package Workshop
  */
 
-get_header(); ?>
+get_header( 'home' ); ?>
 
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area" style="width:30em; margin: auto;">
 		<main id="main" class="site-main" role="main">
+<h1>
+	Send me your project!
+</h1>
+<h2>
+	Applying takes just minutes and you get instant access to world-class freelancers and studios (without the spam). Starting at $50.
+</h2>
+<p><strong>Rules for posting a project:</strong></p>
+<p>This service is ideal for finding the best freelancer or agency you'll ever work with but here's a few qualifications you must have to apply:</p>
+<ul style="width:25em; margin: auto;">
+<li>You want to only be contacted by a handful of talented freelancers that specialize in your specific type of project.</li>
 
+<li>You don’t mind paying for someone who will get the job done right, and you have a budget of over $1,000.</li>
+
+<li>You know that the best possible talent may not be in your 10 mile radius, so you don't care where your freelancer works from.</li>
+
+<li>You don’t want to deal with spam from posting your project online.</li>
+</ul>
+<p><strong>Don't apply if...</strong></p>
+
+<ul style="width:25em; margin: auto;">
+<li> You just want to reach a ton of name-less freelancers. or you want to be saddled with 73 applications for your job that will take hours to sort through. For that, try odesk. 
+</li>
+</ul>
+<div class="box-shadow">
 <div id="wufoo-zk3c41g1wyrseh">
 Fill out my <a href="https://letsworkshop.wufoo.com/forms/zk3c41g1wyrseh">online form</a>.
 </div>
-<div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">There are tons of <a href="http://www.wufoo.com/features/">Wufoo features</a> to help make your forms awesome.</div>
 <script type="text/javascript">var zk3c41g1wyrseh;(function(d, t) {
 var s = d.createElement(t), options = {
 'userName':'letsworkshop',
 'formHash':'zk3c41g1wyrseh',
 'autoResize':true,
-'height':'1087',
+'height':'1794',
 'async':true,
 'host':'wufoo.com',
 'header':'show',
@@ -39,23 +61,37 @@ var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') retu
 try { zk3c41g1wyrseh = new WufooForm();zk3c41g1wyrseh.initialize(options);zk3c41g1wyrseh.display(); } catch (e) {}};
 var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
 })(document, 'script');</script>
+</div>
 
+<h3>People who recently used Workshop-exclusive listings:</h3>
+		<div class="box-shadow">
+			<p class="center">
+				&ldquo;I posted job requests in every place I could find (Twitter, forums, G+ groups, etc) and Workshop was the only one that yielded any decent leads.&rdquo;
+			</p><span class="quote-arrow"></span>
+		</div>
+		<p class="center">
+			<img src="/images/testimonials/joe-walnes.jpg" class="testimonial-img"> – Joe Walnes, Founder and Engineer, <img src="../images/testimonials/nova.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Nova
+		</p>
+		<div class="box-shadow">
+			<p class="center">
+				&ldquo;I heard back from only top professionals and quickly Skyped with 3 experienced developers within 24 hours. I didn't have to deal with sorting through spam from amateur freelancers in order to close the deal with a great freelancer quickly.&rdquo;
+			</p><span class="quote-arrow"></span>
+		</div>
+		<p class="center">
+			<img src="/images/testimonials/marcella.png" class="testimonial-img"> – Marcella Chamorro, Founder and Creative Director, <img src="../images/testimonials/devise.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Nova
+		</p>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<div class="box-shadow">
+			<p class="center">
+				&ldquo;I had a great response from my listing on Workshop. I had 15 top freelancers contact me within a couple hours and the quality of work was very high. I would've wasted a lot more time and effort finding someone if I had gone the traditional job board route - instead found a solid designer quick!&rdquo;
+			</p><span class="quote-arrow"></span>
+		</div>
+		<p class="center">
+			 – Jeff Clark, Founder and Wordpress Developer, <img src="../images/testimonials/jeffrey-dev.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Jeffrey Dev
+		</p>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<hr>
+<?php get_footer( 'home' ); ?>
