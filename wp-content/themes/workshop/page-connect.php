fb2e65974d79298921c0081a16400f8378ba0e59
Robert Williams <robwilliamsgd@gmail.com>
2014-07-20 14:40:14 -0700

    new connect page

--- wp-content/themes/workshop/page-connect.php
+++ wp-content/themes/workshop/page-connect.php
@@ -13,16 +13,100 @@ Template Name: Post to Workshop
  * @package Workshop
  */
 
-get_header( 'home' ); ?>
+get_header( 'connect' ); ?>
+<div style="width:40em; margin: -2em auto 3em auto; display: table; border-bottom: 1px solid #D9D3CF;">
 
 
-	<div id="primary" class="content-area" style="width:30em; margin: auto;">
+	<div style="width:18%; float: left; display:inline-block; vertical-align: 3em;">
+		<p>
+			<strong style="font-weight:900; font-size: 1em;"><img src="/images/logo/workshop-hd-100.png" style="width: 2em; display: inline-block; vertical-align: -.5em; margin-right: .3em;"><?php bloginfo( 'name' ); ?></strong>
+		</p>
+	</div>
+	<div style="width: 55%; display:inline-block;">
+		<p style="padding: 0 0 0 2em; margin: .6em 0 1em 0; display: inline-block;">
+			Send your project directly to the inbox of 200+ world-class freelancers and studios.
+
+		</p>
+	</div>		
+		<div style="width: 25%; text-align: center; float: right; display: inline; font-size: .75em; margin-top: 1.4em;">
+			<a href="https://letsworkshop.wufoo.com/forms/fill-out-this-form-to-post-your-project/"><input type="submit" name="submit" value="POST A JOB FOR FREE"></a>
+		</div>
+	
+</div>
+
+
+
+	<div id="primary" class="content-area connect-wrap">
 		<main id="main" class="site-main" role="main">
-<h1>
-	Send me your project!
-</h1>
+<h2 style="font-size: 2.5em;">
+	"I just want to get this <em>built</em> already."
+</h2><!--
+	<img src="/images/testimonials/marcie-bg.jpg" style="display: inline; left: 15%; top: 50%; position: fixed; transform:rotate(-4deg);
+-ms-transform:rotate(-4deg); /* IE 9 */
+-webkit-transform:rotate(-4deg); z-index: -1;">-->
+
+		<center><p style="margin: -2em 0 0 0;">
+	<img src="/images/testimonials/marcella.png" class="testimonial-img"> – Marcella Chamorro, Founder, <img src="../images/testimonials/devise.png" style="width:20px; border-radius:3px; vertical-align: -2px; "> Devise
+
+</p></center>
+
+<div class="connect-letter">
+	<span class="quote-arrow2"></span>
+
+
+
+<p>
+	That&#39;s how I felt back in December.
+</p>
+<p>
+	I was finished brainstorming my web app <a href="http://deviseapp.com" style=" font-weight: 500;"> Devise</a> but felt stuck.
+</p>
+<h3>
+	I had everything in place. Everything except someone to actually build my app.
+</h3>
+<p>
+	That's when I realized that the wonderful app I had in my head didn't exist yet.
+</p>
+<p>
+	I decided to use Workshop to find a freelancer; and only a few hours after posting my project emails began to boomerang back to my inbox with developer introductions, core competencies, and invites to chat on Skype.
+</p>
+<p>
+	It only took a day to go from feeling lost, with no lead developer to interviewing tons of potential freelancers.
+</p>
+<p>
+	On the first day, I spoke to five developers who sent me proposals.
+</p>
+
+
+<h3>
+	Just one month later, my project had come to life.
+</h3>
+<a href="http://deviseapp.com"><img style="max-width: 290px; margin-right: 1em; margin-left: -8em; display: inline-block;" align="left" src="/images/testimonials/devise-site.png"></a>
+<p>
+	I now have a working product and have since dipped back into my inbox to reach out other developers when I need extra help.
+</p>
+<p>
+	It&#39;s a huge relief to know that I can continue growing Devise by hiring through Workshop in the future.
+</p>
+<p>
+	Thanks to Workshop I was able to bring my project to life.
+</p>
+<p>
+	If you&#39;re looking to find a developer or designer, I highly recommend getting in touch below.
+</p>
+
+<p>– Marcie</p>
+
+
+<!--
+		<div class="box-shadow">
+			<h2 class="center" style="max-width:32em; margin: auto;">
+				&ldquo;I heard back from only top professionals and quickly Skyped with 3 experienced developers within 24 hours.&rdquo;
+			</h2><span class="quote-arrow"></span>
+		</div>
+
 <h2>
-	Applying takes just minutes and you get instant access to world-class freelancers and studios (without the spam). Starting at $50.
+	Instead, I send your project to pre-qualified high-value freelancers and consultants via my daily newsletter. Starting at <strike>$50</strike> <strong>free for sidebar members (limited time)</strong>.
 </h2>
 <p><strong>Rules for posting a project:</strong></p>
 <p>This service is ideal for finding the best freelancer or agency you'll ever work with but here's a few qualifications you must have to apply:</p>
@@ -38,30 +122,9 @@ get_header( 'home' ); ?>
 <p><strong>Don't apply if...</strong></p>
 
 <ul style="width:25em; margin: auto;">
-<li> You just want to reach a ton of name-less freelancers. or you want to be saddled with 73 applications for your job that will take hours to sort through. For that, try odesk. 
+<li> You just want to reach a ton of name-less freelancers. or you want to be saddled with 73 applications for your job that will take hours to sort through. For that, try odesk.
 </li>
 </ul>
-<div class="box-shadow">
-<div id="wufoo-zk3c41g1wyrseh">
-Fill out my <a href="https://letsworkshop.wufoo.com/forms/zk3c41g1wyrseh">online form</a>.
-</div>
-<script type="text/javascript">var zk3c41g1wyrseh;(function(d, t) {
-var s = d.createElement(t), options = {
-'userName':'letsworkshop',
-'formHash':'zk3c41g1wyrseh',
-'autoResize':true,
-'height':'1794',
-'async':true,
-'host':'wufoo.com',
-'header':'show',
-'ssl':true};
-s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
-s.onload = s.onreadystatechange = function() {
-var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-try { zk3c41g1wyrseh = new WufooForm();zk3c41g1wyrseh.initialize(options);zk3c41g1wyrseh.display(); } catch (e) {}};
-var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-})(document, 'script');</script>
-</div>
 
 <h3>People who recently used Workshop-exclusive listings:</h3>
 		<div class="box-shadow">
@@ -72,14 +135,6 @@ var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s
 		<p class="center">
 			<img src="/images/testimonials/joe-walnes.jpg" class="testimonial-img"> – Joe Walnes, Founder and Engineer, <img src="../images/testimonials/nova.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Nova
 		</p>
-		<div class="box-shadow">
-			<p class="center">
-				&ldquo;I heard back from only top professionals and quickly Skyped with 3 experienced developers within 24 hours. I didn't have to deal with sorting through spam from amateur freelancers in order to close the deal with a great freelancer quickly.&rdquo;
-			</p><span class="quote-arrow"></span>
-		</div>
-		<p class="center">
-			<img src="/images/testimonials/marcella.png" class="testimonial-img"> – Marcella Chamorro, Founder and Creative Director, <img src="../images/testimonials/devise.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Nova
-		</p>
 
 		<div class="box-shadow">
 			<p class="center">
@@ -89,9 +144,25 @@ var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s
 		<p class="center">
 			 – Jeff Clark, Founder and Wordpress Developer, <img src="../images/testimonials/jeffrey-dev.png" style="width:20px; border-radius:3px; vertical-align: -2px;"> Jeffrey Dev
 		</p>
-
+</div>-->
 
 		</main><!-- #main -->
+
+		<center>
+			<h3 style="margin: 2em 0 -.75em 0;">
+				Find the most qualified freelancers and consultancies  for your project today.
+			</h3>
+				<p style="color:#9F9D99;">Post your project in minutes. If you get a single spammy reply, I'll pay to post your project elsewhere.</p>
+		</center>
+	<div style="text-align: center; margin: 2em auto; max-width: 20em;">
+		<a href="https://letsworkshop.wufoo.com/forms/fill-out-this-form-to-post-your-project/"><input type="submit" name="submit" value="POST A JOB FOR FREE" data-drip-attribute="sign-up-button"></a>
+	</div>
+
+
+
+<div style="width:40em; margin:auto;">
+
+
+	<hr> </div>
+	<p class="masthead" style="margin: 3em 0 5em 0;">Are you a freelancer? <a href="http://letsworkshop.com">Click here to get leads from people like Marcella</a>.</p>
 	</div><!-- #primary -->
-<hr>
-<?php get_footer( 'home' ); ?>
