eb628326e856eed0dc91da35cfe0a0deede57fa8
Robert Williams <robwilliamsgd@gmail.com>
2014-09-24 11:37:28 -0700

    update bottom of single post

--- wp-content/themes/workshop/content-single.php
+++ wp-content/themes/workshop/content-single.php
@@ -21,23 +21,47 @@
 
 
 
-	<section class="big-wrap" style="display:table; margin-bottom: 2em; font-family: proxima-nova, sans-serif;">
+	<section class="big-wrap" style="display:table; margin: 2em auto; ">
 		<hr>
+		<section class="big-wrap" style="font-family: proxima-nova, sans-serif;">
 			<h3>
 				Thanks for reading <em><?php the_title(); ?></em>
 			</h3>
 			<p>
 
-				If you got value from it, and would like to see me keep writing stuff like it every week, the best thing you can do to ensure this happens is pay me. You will receive future updates and support me in continuing this project.<br><a href="https://workshop.memberful.com/checkout?plan=2993">Click here to subscribe.</a>
+				If you got value from the article, please share it with someone you know. However, it's pretty long so instead of just sharing it with your &ldquo;network&rdquo;, try highlighting only a portion, and sending that to somebody you know. Thanks.
 			</p>
+
+		</section>
+
+				<form class="weekly" action="http://letters.letsworkshop.com/t/i/s/qhdij/" method="post">
 			<p>
-				Secondly, if this essay really had an effect on you, please share it. However, this is a long read that takes some investment on the reader's part so don't just share it with your "network". Instead, highlight a single portion, and send that to somebody you know will benefit.
+				<b>I write one of these every week. Sign up to get them:</b>
 			</p>
-			<p>
-				Lastly, if you liked this and want to read more, <a href="/weekly">you should sign up here</a>.
-			</p>
-			<p>Cheers, Robert</p>
+	    <p>
+	        <input id="fieldName" name="cm-name" type="text" placeholder="Your Name" />
+	    </p>
+	    <p>
+	        <input id="fieldEmail" name="cm-qhdij-qhdij" type="email" required placeholder="Your Email"/>
+	    </p>
+	    <p>
+	        <button type="submit">Send me more</button>
+	    </p>
+	</form>
+			<hr>
 	</section>
+
+	<section class="big-wrap">
+
+
+</section>
+
+<section class="big-wrap">
+
+	<center>
+		<h3 style="margin-bottom: 2em;"><a style="color: #111;" href="/weekly">&larr; Back to the Web as a Sales Letter</a></h3>
+	</center>
+</section>
 
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
