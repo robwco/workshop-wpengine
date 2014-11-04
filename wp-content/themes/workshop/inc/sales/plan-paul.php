cb5b405967398abe75c9a81a5b8aa29d774118b3
Robert Williams <robwilliamsgd@gmail.com>
2014-11-04 10:05:40 -0800

    paul jarvis landing page

---
+++ wp-content/themes/workshop/inc/sales/plan-paul.php
@@ -0,0 +1,7 @@
+	<p class="center">
+		12 payments of <strike>$64</strike> $50/month (Over 20% off -- a special Paul Jarvis discount)
+	</p>
+
+	<div style="text-align: center; margin: 2em;">
+		<a href="https://workshop.memberful.com/checkout?plan=3618"><input type="submit" name="submit" value="SEND ME 2400+ LEADS A YEAR" data-drip-attribute="sign-up-button"></a>
+	</div>
